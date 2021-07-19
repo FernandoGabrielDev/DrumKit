c56834e372e1fb39d39435b1c1418f20af8f272a
Fernando Gabriel Tupina Lima <fernando-gabriel16@outlook.com>
2021-07-19 13:39:48 -0300

    Finalização da primeira aula do desafio "7 Projetos em 7 Dias" promovido pela B7Web

---
+++ script.js
@@ -0,0 +1,44 @@
+document.body.addEventListener('keyup', (event) => {
+    //console.log(event.code);
+    playSound(event.code.toLowerCase());
+});
+
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+    //console.log("Música", song);
+
+    if(song !== ''){
+        let songArray = song.split(''); //separando a string em um array
+        //console.log(songArray);
+        playComposition(songArray);
+    }
+});
+
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if(audioElement){
+        audioElement.currentTime = 0;
+        audioElement.play(); //função para tocar o audio da TAG AUDIO
+    }
+
+    if(keyElement){
+        keyElement.classList.add('active'); //função para adicionar uma class do CSS (definida previamente)
+        setTimeout(() => {
+            keyElement.classList.remove('active'); //função para remover a class adicionada anteriormente
+        }, 300); //tempo para a função iniciar (em milissegundos)
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+   
+    for(let songItem of songArray){
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+        }, wait);
+
+        wait += 250;
+    }
+}
